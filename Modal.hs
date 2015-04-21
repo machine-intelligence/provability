f0fe7c838d4f9f31b28f606a9ed38876255b79e0
nate <nate@so8r.es>
2015-04-21 14:35:24 -0700

    Added comment to help de-confuse <1>.

--- Modal.hs
+++ Modal.hs
@@ -64,6 +64,9 @@ holdsk k phi = Neg (incon k) `Imp` phi
 boxk :: Int -> ModalFormula v -> ModalFormula v
 boxk k phi = Box (holdsk k phi)
 
+-- <1> a is NOT the same as ~[1]~ a.
+-- The former is ~[]~ (~[]F -> phi)
+-- The latter is ~[] (~[]F -> ~phi)
 diak :: Int -> ModalFormula v -> ModalFormula v
 diak k phi = Dia (holdsk k phi)
 
