718a2a0f6046dcdc61f36ce05ff1602b2d784997
nate <nate@so8r.es>
2015-04-22 22:08:19 -0700

    Added utilities to make it easier to watch the Kripke frames stabalize

--- Display.hs
+++ Display.hs
@@ -1,31 +1,60 @@
 module Display where
-import Data.Map hiding (map, foldr)
+import Data.Map (Map, (!), keys, toAscList)
+import qualified Data.Map as Map
+import Data.List (transpose)
 
 type Table = [[String]]
 
-mapListToTable :: (Ord k, Enum k, Show k, Show v) => [k] -> [Map k v] -> Table
-mapListToTable [] _    = []
-mapListToTable ks maps = header : rows where
-  header = "":"│":map show ks
-  rows = [show n : "│" : map (show . (m !)) ks | (n, m) <- zip [0..] maps]
+padr :: a -> Int -> [a] -> [a]
+padr x n xs = xs ++ replicate (n - length xs) x
+
+padl :: a -> Int -> [a] -> [a]
+padl x n xs = replicate (n - length xs) x ++ xs
+
+modalBool :: Bool -> String
+modalBool True = "⊤"
+modalBool False = "⊥"
+
+kripkeTable' :: (Show k, Ord k) => [k] -> Map k [Bool] -> Table
+kripkeTable' ks m = listmapToTable ks $ Map.map (map modalBool) m
+
+kripkeTable :: (Show k, Ord k) => Map k [Bool] -> Table
+kripkeTable m = kripkeTable' (keys m) m
+
+listmapToTable :: (Show k, Ord k) => [k] -> Map k [String] -> Table
+listmapToTable [] _ = []
+listmapToTable ks m = header : rows where
+  header = "" : "│" : map show ks
+  unpaddedCols = map (m !) ks
+  cols = map (padr "" $ maximum $ map length unpaddedCols) unpaddedCols
+  rows = zipWith addNum [0..] (transpose cols)
+  addNum n row = show n : "│" : row
 
 mapToTable :: (Ord k, Show k, Show v) => Map k v -> Table
-mapToTable = map (\(k, v) -> [show k, " :  ", show v]) . toAscList
+mapToTable = map (\(k, v) -> [show k, ": ", show v]) . toAscList
 
 displayMap :: (Ord k, Show k, Show v) => Map k v -> IO ()
 displayMap = displayTable . mapToTable
 
-squareUp :: Table -> [[String]]
-squareUp rows = map normalizeRow paddedRows where
-  paddedRows = map (pad "" $ maxlen rows) rows
-  pad x n xs = xs ++ replicate (n - length xs) x
+squareUp' :: String -> String -> Table -> [[String]]
+squareUp' l r rows = map normalizeRow paddedRows where
+  paddedRows = map (padr "" $ maxlen rows) rows
   maxlen = foldr (max . length) 0
   normalizeRow row = zipWith normalizeCell [0..] row where
-    normalizeCell i = pad ' ' (colwidth i)
+    normalizeCell i c = l ++ (padl ' ' (colwidth i) c) ++ r
   colwidth i = maximum [length $ r !! i | r <- paddedRows]
+
+squareUp :: Table -> [[String]]
+squareUp = squareUp' " " " "
 
 renderTable :: Table -> String
 renderTable table = unlines $ map concat (squareUp table)
 
 displayTable :: Table -> IO ()
 displayTable = putStrLn . renderTable
+
+displayKripkeFrames' :: (Show k, Ord k) => [k] -> Map k [Bool] -> IO ()
+displayKripkeFrames' ks = displayTable . kripkeTable' ks
+
+displayKripkeFrames :: (Show k, Ord k) => Map k [Bool] -> IO ()
+displayKripkeFrames m = displayKripkeFrames' (keys m) m
