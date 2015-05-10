28227e8bda7026b596d3a5d3132e94f047e8cce2
nate <nate@so8r.es>
2015-05-10 09:29:35 +0100

    Added modal combat program.

---
+++ src/ModalCombat.hs
@@ -0,0 +1,42 @@
+module Main where
+import Data.Monoid
+import Data.Foldable
+import Options.Applicative
+import System.Environment (getProgName)
+import Modal.Code
+import Modal.Combat
+import Modal.Environment
+import Modal.Utilities
+
+programDescription :: String
+programDescription =
+  "Modal combat checker. Figures out how modal agents will behave " <>
+  "in the prisoner's dilemma with shared source code."
+
+data Options = Options
+  { optEnvs :: [FilePath]
+  , optFile :: FilePath
+  } deriving Show
+
+optionParser :: Parser Options
+optionParser = Options
+  <$> many (option str
+    (  long "env"
+    <> short 'e'
+    <> metavar "FILE"
+    <> help "An environment file defining other agents." ))
+  <*> argument str
+    ( metavar "FILE" )
+
+options :: String -> ParserInfo Options
+options name = info (helper <*> optionParser)
+  (  fullDesc
+  <> progDesc programDescription
+  <> header (name ++ " - Modal Combat!" ) )
+
+main = do
+  name <- getProgName
+  opts <- execParser $ options name
+  bases <- mapM compileFile (optEnvs opts)
+  env <- run (foldlM insertAll nobody bases) :: IO (Env CD CD)
+  playFile (optFile opts) env
